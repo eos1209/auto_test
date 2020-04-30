38c5d5f7533fd0a640f5d7c150d3d6c5b86c2aea
loka <carky5566@gmail.com>
2020-04-30 18:08:33 +0800

    fix commissionSetting

--- case/test_api/test_BaseCase/test_Master_Base_API_CommissionSetting.py
+++ case/test_api/test_BaseCase/test_Master_Base_API_CommissionSetting.py
@@ -24,10 +24,24 @@ class CommissionSettingBaseTest(unittest.TestCase):
 
     def getId(self):
         # 取得ID
+        getId = 0
         response_data = self.CommissionSetting.getList({})
-        length = len(response_data[1]['CommissionSettings']) - 1
-        getId = response_data[1]['CommissionSettings'][length]['Id']
-        return getId
+        print("QA_automation" + common_config.now)
+        length = len(response_data[1]['CommissionSettings'])
+        for i in range(length):
+            if response_data[1]['CommissionSettings'][i]['Name'] == "QA_automation" + common_config.now:
+                getId = response_data[1]['CommissionSettings'][i]['Id']
+                return getId
+
+    def getDeleteId(self):
+        # 取得ID
+        getId = 0
+        response_data = self.CommissionSetting.getList({})
+        length = len(response_data[1]['CommissionSettings'])
+        for i in range(length):
+            if response_data[1]['CommissionSettings'][i]['Name'] == 'QA_automation_Modify' + common_config.now:
+                getId = response_data[1]['CommissionSettings'][i]['Id']
+                return getId
 
     def test_CommissionSetting_relatedApi_status_01(self):
         """驗證 佣金設定 - 取得列表頁面"""
@@ -49,8 +63,10 @@ class CommissionSettingBaseTest(unittest.TestCase):
 
     def test_CommissionSetting_relatedApi_status_04(self):
         """驗證 佣金設定 - 新增傭金等級"""
-        data = {"commissionSetting": {"Name": "QA_automation", "MinWager": 1, "DepositPercent": 1, "DepositMax": 1,
+        data = {"commissionSetting": {"Name": "QA_automation" + common_config.now, "MinWager": 1, "DepositPercent": 1,
+                                      "DepositMax": 1,
                                       "WithdrawPercent": 1, "WithdrawMax": 1, "DepositThreshold": 1}}
+        print("QA_automation" + common_config.now)
         response_data = self.CommissionSetting.createSubmit(data)
         status_code = response_data[0]
         self.assertEqual(status_code, common_config.Status_Code)
@@ -67,7 +83,7 @@ class CommissionSettingBaseTest(unittest.TestCase):
         getId = self.getId()
         data = {"id": getId}
         response_data = self.CommissionSetting.getDetail(data)
-        # print(response_data[1])
+        print(response_data[1])
         status_code = response_data[0]
         self.assertEqual(status_code, common_config.Status_Code)
 
@@ -92,20 +108,19 @@ class CommissionSettingBaseTest(unittest.TestCase):
         """驗證 佣金設定 - 更新佣金設定"""
         # Step 1 取得ID
         getId = self.getId()
-        data = {"commissionSetting": {"Id": getId, "Name": 'QA_automation' + common_config.now, "IsDisabled": 'false',
+        data = {"commissionSetting": {"Id": getId, "Name": 'QA_automation_Modify' + common_config.now,
+                                      "IsDisabled": 'false',
                                       "MinWager": 1,
                                       "DepositThreshold": 1, "DepositPercent": 1, "DepositMax": 1, "WithdrawPercent": 2,
                                       "WithdrawMax": 1, "SettingDetails": [], "AgentCount": 0}}
         response_data = self.CommissionSetting.update(data)
-        # print(response_data[1])
         status_code = response_data[0]
         self.assertEqual(status_code, common_config.Status_Code)
 
     def test_CommissionSetting_relatedApi_status_10(self):
         """驗證 佣金設定 - 刪除佣金設定"""
         # Step 1 取得ID
-        getId = self.getId()
-        print(getId)
+        getId = self.getDeleteId()
         data = {"id": getId}
         response_data = self.CommissionSetting.delete(data)
         # print(response_data[1])
